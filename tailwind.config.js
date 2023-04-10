400fbf3c0be23e856f61f823afc16f134ccca298
Emre Erden <emreerden@pm.me>
2023-04-10 18:55:05 +0300

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
