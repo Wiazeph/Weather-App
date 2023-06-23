1565ca7086c1a6cd212a4160c8da57395f6f44bc
Emre Erden <emreerden@pm.me>
2023-06-24 02:19:53 +0300

    feat: add getLocationData action

--- src/stores/WeatherDataStore.js
+++ src/stores/WeatherDataStore.js
@@ -19,9 +19,36 @@ export const useWeatherStore = defineStore('weather', {
 
         this.isLoading = false
 
-        console.log(response.data)
+        console.log('getWeatherData ----->', response.data)
       } catch (error) {
         console.error(error)
+      }
+    },
+
+    async getLocationData() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            try {
+              const latitude = position.coords.latitude
+              const longitude = position.coords.longitude
+              const response = await axios.get(
+                `https://api.weatherapi.com/v1/forecast.json?key=7b2f4b0de753442aa37235151231706&q=${latitude},${longitude}&days=3`
+              )
+
+              this.weatherData = response.data
+
+              console.log('getLocationData ----->', response.data)
+            } catch (error) {
+              console.error(error)
+            }
+          },
+          (error) => {
+            alert('Failed to get location: ' + error.message)
+          }
+        )
+      } else {
+        alert('Geolocation is not supported by this browser.')
       }
     }
   }
